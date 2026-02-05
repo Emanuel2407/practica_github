a5e367a3ca2e07a6f90e69de9482b738a3fc5568
Emanuel2407 <emanuelatencia1126654@correo.itm.edu.co>
2026-02-05 11:39:12 -0500

    feat:Creación de main con función crear

---
+++ main.py
@@ -0,0 +1,15 @@
+def sumar_dos_numreos(numero1: int, numero2: int) -> int:
+    """
+    Suma dos números enteros y devuelve el resultado.
+
+    Args:
+        numero1 (int): Primer número a sumar.
+        numero2 (int): Segundo número a sumar.
+
+    Returns:
+        int: Resultado de la suma de ambos números.
+    """
+    return numero1 + numero2
+
+
+print(sumar_dos_numreos(2, 1))
